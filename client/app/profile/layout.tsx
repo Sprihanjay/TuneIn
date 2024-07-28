2e6e3de84648105eb0728d11be3ede0bd1429a45
Michael Wu <michaelwu2004@gmail.com>
2024-07-28 12:12:32 -0500

    Set up profile page with dummy components

---
+++ client/app/profile/layout.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import { SignIn } from "../components/signin/signin";
+import useAuth from "@/lib/hooks/useAuth";
+import { Navbar } from "../components/navbar";
+
+const ProfileLayout = ({ children }: { children: React.ReactElement }) => {
+  const isAuthenticated = useAuth();
+
+  return isAuthenticated ? (
+    <>
+      <Navbar />
+      {children}
+    </>
+  ) : (
+    <main className="flex justify-center items-center h-screen">
+      <SignIn />
+    </main>
+  );
+};
+
+export default ProfileLayout;
---
+++ client/app/profile/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Image from "next/image";
+import { Tabs } from "../components/ui/tabs";
+import { tabs } from "@/lib/constants";
+
+export default function Profile() {
+  return (
+    <div className="bg-customone min-h-screen h-full md:h-[40rem] [perspective:1000px] relative b flex flex-col mx-auto w-full  items-start justify-start0 px-10 pb-40">
+      <Tabs tabs={tabs} />
+    </div>
+  );
+}
