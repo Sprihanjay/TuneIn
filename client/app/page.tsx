36c514a0d0be47f82714a0746d0ee82987f1a097
Kenny <kcao0918@gmail.com>
2024-07-27 23:27:12 -0400

    added basic fb storage to createPost

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
 import { SignIn } from "./components/signin/signin";
+import useAuth from "@/lib/hooks/useAuth";
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-screen flex-col">
-      <SignIn />
+    <main>
+      <h1>Test</h1>
+      <Link href="/dashboard">
+        <button>Apples</button>
+      </Link>
     </main>
   );
 }
